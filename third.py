59b70bd6a6acacc8f4c84e9949ce2eafe99304cf
venky4588 <venkateshprem4588@gmail.com>
2023-03-27 17:22:49 +0530

    python files

---
+++ third.py
@@ -0,0 +1,11 @@
+a = 300
+b = 200
+if a > b :
+    print ("a is big")
+else :
+    print ("bi is big")
+    
+# thi is my programs 
+""" this 
+is 
+my """
---
+++ venky.py
@@ -0,0 +1,23 @@
+'''a = 200
+b = 100
+c = 600
+d = 40
+total = ( a + b + c + d )
+oneguide = 50
+print ( total/oneguide )
+name = 'avula venkatesh'
+print (name)
+'''
+
+#functions
+
+def  venkyBest(book):
+    if (book%2==0) :
+        print('{} is even' .format(book))
+    else:
+        print ('{} is odd '.format(book))
+ 
+venkyBest(10)
+venkyBest(77)
+venkyBest(1033)
+venkyBest(104)
